63200720f7d0854af7466607ddd8128f7231c54b
ponbiki <ponbiki@beryllium>
2015-07-13 08:57:58 -0400

    Attempts at routing

--- app/routes/home.php
+++ app/routes/home.php
@@ -16,6 +16,21 @@ if ($loggedin) {
     }
 }
 
+$app->get('/', function() use ($app) {
+    
+    $page = "FTW Log In";
+    $meta = "";
+    $error;
+    
+    $app->render('home.html.twig', [
+        'page' => $page,
+        'meta' => $meta,
+        'error' => $error
+    ]);
+})->name('home');
+
+unset($error);
+
 $app->post('/', function () use ( $app ) {
 
     $user = filter_var(($app->request()->post('user')), FILTER_SANITIZE_STRING);
@@ -26,20 +41,7 @@ $app->post('/', function () use ( $app ) {
     } else {
         $con = new ftw\Database();
         $error[] = $con->auth($user, $pass);
+    //    $url = $app->urlFor('/:error', array('error' => $error));
+    //    $app->redirect($url);
     }
 });
-
-$app->get('/', function() use ($app) {
-    
-    $page = "FTW Log In";
-    $meta = "";
-    global $error;
-    
-    $app->render('home.html.twig', [
-        'page' => $page,
-        'meta' => $meta,
-        'error' => $error
-    ]);
-})->name('home');
-
-unset($error);
