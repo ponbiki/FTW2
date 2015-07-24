521e76444ca4ce24a4d35aae7fd1a79c203af129
ponbiki <ponbiki@beryllium>
2015-07-23 22:27:34 -0400

    cleaned superfluous whitespace from Version class

--- src/Version.php
+++ src/Version.php
@@ -9,17 +9,17 @@ class Version implements iVersion
         //returns TRUE if master and workspace are at the same ver
         //returns FALSE if master and workspace are not at the same ver
     }
-    
+
     public static function pull()
     {
         //pulls latest master to local workspace
     }
-    
+
     public static function commit()
     {
         //commits latest change in workspace
     }
-    
+
     public static function push()
     {
         //pushes latest commit to master
