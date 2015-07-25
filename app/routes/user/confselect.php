c8f031d0a29c4c9e22c25c33a2391e13f2604876
ponbiki <ponbiki@beryllium>
2015-07-24 22:10:53 -0400

    user/confselect logic

--- app/routes/user/confselect.php
+++ app/routes/user/confselect.php
@@ -17,17 +17,20 @@ $app->post('/user/confselect', function () use ($app) {
         $_SESSION['error'][] = "Please select a valid configuration file";
         $app->redirect('/user/menu');
     } else {
-        if (ftw\Version::chkVersions() !== TRUE) {
-            ftw\Version::pull();
+        $_SESSION['info'][] = "Conf is in the array";
+        if (/*ftw\Version::chkVersions()*/TRUE !== TRUE) {
+            //ftw\Version::pull();
             // if confpull method gets no conf file some error
             // else cp to a dated stored backup
             // (probably in sql order by date hold 5
             // then delete oldest and add new thereafter)
             // as well as a working copy for regex attack
+        } else {
+            $_SESSION['info'][] = $_SESSION['conftype'][$conf];
         }
     }
-
-    $_SESSION[info][] = "It worked";
+    
+    $_SESSION['info'][] = "It worked";
     $app->redirect('/user/menu');
 
 });
