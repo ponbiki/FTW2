ca9f40ff94b9db4e59be960a967f34ba809f1c6b
ponbiki <ponbiki@beryllium>
2015-08-10 00:46:30 -0400

    origin location and server extraction completed in BasConf::loadConf()

--- src/BasConf.php
+++ src/BasConf.php
@@ -16,21 +16,25 @@ class BasConf implements iConf
             $con->confBackup($conf, $file);
             $file_array = explode(PHP_EOL, $file);
             foreach ($file_array as $index => $value) {
-                if (preg_match('/^\$name\s*=+/', $value)) {
+                if (preg_match('/^\$name\s*=/', $value)) {
                     preg_match('/[\'|\"](.*)[\'|\"]/', $value, $name);
                     $confvals['name'] = $name[1];
-                } elseif (preg_match('/^\$hostname\[\]\s*=+/', $value)) {
+                } elseif (preg_match('/^\$hostname\[\]\s*=/', $value)) {
                     preg_match('/[\'|\"](.*)[\'|\"]/', $value, $hostname);
                     $confvals['hostname'][] = $hostname[1];
-                } elseif (preg_match('/^\$sslhostname\[\]\s*=+/', $value)) {
+                } elseif (preg_match('/^\$sslhostname\[\]\s*=/', $value)) {
                     preg_match('/[\'|\"](.*)[\'|\"]/', $value, $sslhostname);
                     $confvals['sslhostname'][] = $sslhostname[1];
-                } elseif (preg_match('/^\$checkurl\s*=+/', $value)) {
+                } elseif (preg_match('/^\$checkurl\s*=/', $value)) {
                     preg_match('/[\'|\"](.*)[\'|\"]/', $value, $checkurl);
                     $confvals['checkurl'] = $checkurl[1];
-                } elseif (preg_match('/^\$checkhost\s*=+/', $value)) {
+                } elseif (preg_match('/^\$checkhost\s*=/', $value)) {
                     preg_match('/[\'|\"](.*)[\'|\"]/', $value, $checkhost);
                     $confvals['checkhost'] = $checkhost[1];
+                } elseif (preg_match('/^\$origin\[[\'|\"].*[\'|\"]\]\s*=/', $value)) {
+                    preg_match('/\[[\'|\"](.*)[\'|\"]\]/', $value, $origin_id);
+                    preg_match('/=\s*[\'|\"](.*)[\'|\"]/', $value, $server);
+                    $confvals['origin'][$origin_id[1]] = $server[1];
                 }
             }
             return $confvals;
